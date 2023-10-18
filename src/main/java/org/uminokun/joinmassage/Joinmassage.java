493e74c84041abca919509206b9d78332e37ec99
uminokun <info@kakkaserver.net>
2023-10-18 20:38:06 +0900

    test plugins part1 new plugins project. new commands+1 enable to massage disable to massage

--- src/main/java/org/uminokun/joinmassage/Joinmassage.java
+++ src/main/java/org/uminokun/joinmassage/Joinmassage.java
@@ -7,11 +7,28 @@ public final class Joinmassage extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("onEnableメソッドが呼び出されたよ！！");
     }
 
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("test")) {
+            // プレイヤーが /basic を実行すると、この部分の処理が実行されます...
+            return true;
+        } else if (cmd.getName().equalsIgnoreCase("basic")) {
+            // プレイヤーが /basic2 を実行すると、この部分の処理が実行されます...
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("このコマンドはゲーム内から実行してください。");
+            } else {
+                Player player = (Player) sender;
+                // コマンドの実行処理...
+            }
+            return true;
+        }
+        return false;
+    }
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("onDisableメソッドが呼び出されたよ！！");
     }
 }
