521a68dae054b34e6ae3ce47f73add18ee04bc99
uminokun <info@kakkaserver.net>
2023-10-18 20:48:12 +0900

    test plugins part1

--- src/main/java/org/uminokun/joinmassage/Joinmassage.java
+++ src/main/java/org/uminokun/joinmassage/Joinmassage.java
@@ -1,5 +1,8 @@
 package org.uminokun.joinmassage;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Joinmassage extends JavaPlugin {
